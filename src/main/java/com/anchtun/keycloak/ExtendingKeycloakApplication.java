47f7718da9672808031afc1779180982dc4e71ef
anchtun1212 <med.aymen.charrada@gmail.com>
2023-10-08 09:08:14 +0300

    first commit

---
+++ src/main/java/com/anchtun/keycloak/ExtendingKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.anchtun.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendingKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtendingKeycloakApplication.class, args);
+	}
+
+}
