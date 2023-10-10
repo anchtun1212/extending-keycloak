6c838e03ee8a49ddd321fd2a0ea562910c4274b2
anchtun1212 <med.aymen.charrada@gmail.com>
2023-10-10 09:54:41 +0300

    re-organize the project

---
+++ extending-keycloak/src/main/java/com/anchtun/keycloak/demo/DemoProviderFactory.java
@@ -0,0 +1,6 @@
+package com.anchtun.keycloak.demo;
+
+import org.keycloak.provider.ProviderFactory;
+
+public interface DemoProviderFactory extends ProviderFactory<DemoProvider> {
+}
---
+++ extending-keycloak/src/test/java/com/anchtun/keycloak/demo/ExtendingKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anchtun.keycloak.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtendingKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
