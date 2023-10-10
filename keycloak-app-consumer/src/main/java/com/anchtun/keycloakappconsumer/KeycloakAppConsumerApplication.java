01e4398cb8d7b71dd9cc37720eb037d417138359
anchtun1212 <med.aymen.charrada@gmail.com>
2023-10-10 09:56:03 +0300

    add keycloak-app-customer

---
+++ keycloak-app-consumer/src/main/java/com/anchtun/keycloakappconsumer/KeycloakAppConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.anchtun.keycloakappconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakAppConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakAppConsumerApplication.class, args);
+	}
+
+}
---
+++ keycloak-app-consumer/src/test/java/com/anchtun/keycloakappconsumer/KeycloakAppConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anchtun.keycloakappconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAppConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
